5c0199942b3120f32e58d57fa42f1c03d56be1fe
shruti-cs4 <shrutichandravanshi04@gmail.com>
2025-07-04 15:21:16 +0530

    fianl commit

--- Todo.js
+++ Todo.js
@@ -1,28 +1,26 @@
 const todoList = [];
+let htmlTodoList = ``;
 
 function add(){
   const work = document.querySelector('.todo').value;
   const date = document.querySelector('.date').value;
   const todo = [work, date]
   todoList.push(todo);
-  addingList();
-  console.log()
   
 };
 
 function addingList(){
-  let htmlTodoList = ``;
-  for (let i = 0; i < todoList.length; i++){
+  todoList.forEach(function(value, index){
     const html = `
-      <div>${todoList[i][0]}</div>
-      <div>${todoList[i][1]}</div>
-      <button class = delete-button onclick="deleteItem(${i})">
+      <div>${value[0]}</div>
+      <div>${value[1]}</div>
+      <button class = delete-button onclick="deleteItem(${index})">
         Delete
       </button>`;
-    htmlTodoList += html;
-  }
-  document.querySelector('.text').innerHTML = htmlTodoList;  
-};
+    htmlTodoList += html; 
+  })
+  document.querySelector('.text').innerHTML = htmlTodoList;
+}
 
 function deleteItem(i){
   todoList.splice(i, 1);
